cf04f60642147b18aec4cc08a187ed40656213db
PiyushTyagiBDAT <71528379+PiyushTyagiBDAT@users.noreply.github.com>
2020-11-12 14:13:24 +0530

    Add files via upload

---
+++ FTPApp/Models/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FTPApp.Models
+{
+    class Constants
+    {
+        public class FTP
+        {
+            public const string UserName = @"bdat100119f\bdat1001";
+            public const string Password = "bdat1001";
+            public const string CRN = "10983";
+            public const string BaseUrl = "ftp://waws-prod-dm1-127.ftp.azurewebsites.windows.net/bdat1001-10983";
+
+            public const int OperationPauseTime = 10000;
+        }
+    }
+} 
---
+++ FTPApp/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FTPApp.Models
+{
+    class Student
+    {
+        public string StudentId { get;set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string ImageData { get; set; }
+        public bool MyRecord { get; set; }
+
+        public void FromDirectory(string dir) {
+
+            
+            string[] dp = dir.Split(" ", StringSplitOptions.None);
+            StudentId = dp[0];
+           FirstName = dp[1];
+            LastName = dp[2];
+        }
+        public override string ToString()
+        {
+            return $"{StudentId} {LastName}{FirstName}";
+        }
+    }
+}
